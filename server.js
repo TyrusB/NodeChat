1d2f66402c1fb551b958d65aa8914699bb46a9b5
Ty Brooks <brooks85.ty@gmail.com>
2014-03-18 11:46:36 -0700

    Finish phase II

---
+++ server.js
@@ -0,0 +1,19 @@
+var static = require('node-static');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+
+var file = new static.Server('./public');
+
+http.createServer(function (request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response, function(e, res) {
+      if (e && (e.status === 404)) {
+        file.serveFile('/not-found.html', 404, {}, request, response);
+      }
+    });
+  }).resume();
+}).listen(8080);
+
+console.log("server running at localhost:8080");
